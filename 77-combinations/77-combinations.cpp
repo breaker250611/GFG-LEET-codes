9daa1a8b1b64d31b4408aa5dc9c6bfb525626dda
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-04-06 09:21:56 +0530

    Time: 400 ms (21.24%), Space: 164.3 MB (12.69%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>nums;
+        for(int i = 1;i<=n;i++){
+            nums.push_back(i);
+        }
+        
+        vector<vector<int>>ans;
+        combination(0,nums,k,0,ans,{});
+        return ans;
+    }
+    void combination(int i,vector<int>&nums,int k,int csf,vector<vector<int>>&ans,vector<int>temp){
+        if(csf==k){
+            ans.push_back(temp);
+            return;
+            
+        }
+        
+        for(i;i<nums.size();i++){
+            
+            temp.push_back(nums[i]);
+            combination(i+1,nums,k,csf+1,ans,temp);
+            temp.pop_back();
+        }
+    }
+};
