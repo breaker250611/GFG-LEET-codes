8d6ff81b3b609e82066ed2228b34efe48d806f10
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-04-07 13:55:27 +0530

    Time: 50 ms (31.00%), Space: 6.5 MB (20.21%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<vector<char>>ans;
+    void solveSudoku(vector<vector<char>>& board) {
+        // ans.clear;
+        
+        solve(board,0,0);
+        board = ans;
+    }
+    void solve(vector<vector<char>>&board,int i,int j){
+        if(i==board.size()) {
+            for(int x = 0;x<board.size();x++){
+                for(int y = 0;y<board[0].size();y++){
+                    cout<<board[x][y]<<" ";
+                    
+                }
+                cout<<endl;
+            }
+            ans = board;
+            return;
+        }
+        int nayai ;
+        int nayaj ;
+        if(j==board[0].size()){
+            nayai = i+1;
+            nayaj = 0;
+        }
+        else{
+            nayai = i;
+            nayaj = j+1;
+        }
+        
+        if(board[i][j]!='.') solve(board,nayai,nayaj);
+        else{
+            for(char no = 1;no<=9;no++){
+                if(sahihai(board,i,j,no)){
+                    board[i][j]=no+'0';
+                    solve(board,nayai,nayaj);
+                    board[i][j]='.';
+                }
+            }
+        }
+    }
+    bool sahihai(vector<vector<char>>&board,int i,int j,int num){
+        for(int y = 0;y<board[0].size();y++){
+            if(board[i][y]-'0'==(num)) return false;
+        }
+        for(int y = 0;y<board.size();y++){
+            if(board[y][j]-'0'==(num)) return false;
+        }
+
+        int chotai = i/3*3;
+        int chotaj  = j/3*3;
+        for(int a = 0;a<3;a++){
+            for(int b = 0;b<3;b++){
+                if(board[a+chotai][b+chotaj]==(num+'0')) return false;
+            }
+        }
+        return true;
+    }
+};
