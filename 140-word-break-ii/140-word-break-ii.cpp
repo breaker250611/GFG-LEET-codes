145b52c0ec61488e2b8d7701a07ba1783fa59f82
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-04-08 17:51:56 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (64.52%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    map<string,int>mp;
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        mp.clear();
+        for(int i = 0;i<wordDict.size();i++){
+            mp[wordDict[i]]++;
+        }
+        vector<string>ans;
+        karbhaicall(s,ans,"",0);
+        return ans;
+    }
+    
+    void karbhaicall(string s,vector<string>&ans,string psf,int i){
+        
+        if(i==s.size()){
+            psf.pop_back();
+            ans.push_back(psf);
+        }
+        
+        for(int j = i;j<s.size();j++){
+            string abhi = s.substr(i,j-i+1);
+            if(mp.find(abhi)!=mp.end()){
+                // int bada = s.substr(j+1);
+                karbhaicall(s,ans,psf+abhi+" ",j+1);
+            }
+        }
+    }
+};
