183be194385e758d32ab559e8f4b977b9d40d28d
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-04-10 11:36:14 +0530

    Time: 910 ms (9.51%), Space: 35.8 MB (47.87%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,49 +1,37 @@
-#define f first
-#define s second
-#define vpi vector<pair<int , int>>
-#define  pb push_back 
 class Solution {
 public:
-    
-    int func (vpi &ar , int i , int m , int n , int dp[601][101][101]){
-        if ( i >= ar.size()){
-            return 0;
-        }
+    int func(vector<pair<int,int>>& ar,int i , int m , int n , int dp[601][101][101]){
+        if(i>=ar.size()) return 0;
         
-        // if (m < 0 or n < 0) return 0;
-       // if (m == 0 and n == 0) return 0;
-        if (dp[i][m][n] != -1){
+        
+        if(dp[i][m][n]!=-1){
             return dp[i][m][n];
         }
-        
-       int op2 =  0 + func (ar , i+1 , m , n , dp); 
-       int op1 =0;
-       if (m-ar[i].f >= 0 and n-ar[i].s >= 0) {
-           op1  =  1 + func(ar , i+1 , m-ar[i].f , n-ar[i].s , dp);
-       }
-        
-       return dp[i][m][n] = max(op1 , op2); 
-    }
-    
-    
-    
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        vpi ar;
-        for (auto x : strs){
-            int a = 0 , b = 0;
-            for (auto y : x){
-                y == '1' ? a++ : b++;
-            }
-            ar.pb({b , a});
+        int op2 = 0+func(ar,i+1,m,n,dp);
+        int op1 = 0;
+        if(m-ar[i].first>=0 and n-ar[i].second>=0){
+            op1 = 1+ func(ar,i+1,m-ar[i].first,n-ar[i].second,dp);
         }
+        return dp[i][m][n] = max(op1,op2);
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<pair<int,int>>ar;
+        for(auto x:strs){
+            int a = 0,b=0;
+            for(auto &y :x){
+                y=='1'?a++:b++;
+            }
+            ar.push_back({b,a});
+            }
         int dp[601][101][101];
-        for (int i = 0 ; i<=600 ; i++){
-            for (int j = 0 ; j<=100 ; j++){
-                for (int k = 0 ; k <= 100 ; k++){
-                 dp[i][j][k] = -1;
+        for(int i = 0;i<=600;i++){
+            for(int j = 0;j<=100;j++){
+                for(int k = 0;k<=100;k++){
+                    dp[i][j][k] = -1;
                 }
             }
         }
-        return func(ar , 0 , m , n , dp);
+        return func(ar,0,m,n,dp);
     }
+    
 };
