89577de7b71d825c878db99404297bbc4c148f90
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-05-13 18:24:33 +0530

    Time: 303 ms (8.46%), Space: 37.4 MB (92.53%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    void dfs(int node, vector<int> adj[], vector<int> &visi)
-    {
-        visi[node] = 1;
-        for(auto i: adj[node]){
-            if(!visi[i]){
-                dfs(i, adj, visi);
-            }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size()<n-1)
+            return -1;
+        
+        vector<int> parent(n,-1);
+        
+        int count=1;
+        for(int i=0;i<connections.size();i++)
+        {
+            uni(parent,connections[i][0],connections[i][1],count);
         }
+        
+        return n-count;
+        
     }
-    int makeConnected(int n, vector<vector<int>>& edges) {
+    int findAbsParent(vector<int>& parent,int i)
+    {
+        while(parent[i]!=-1)
+            i=parent[i];
+        return i;
+    }
     
-        if(edges.size() < n-1) return -1;
-        vector<int> adj[n];
-        for(auto i: edges){ 
-            int u = i[0];
-            int v = i[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+    void uni(vector<int>& parent,int x,int y,int& count)
+    {
+        int parent1=findAbsParent(parent,x);
+        int parent2=findAbsParent(parent,y);
+        if(parent1!=parent2)
+        {
+            parent[parent1]=parent2;
+            count++;
         }
-        int comp = 0;
-        vector<int> visi(n, 0);
-        for(int i=0; i<n; i++){
-            if(!visi[i]){
-                dfs(i, adj, visi);
-                comp++; 
-            }
-        }
-        return comp-1;
     }
 };
