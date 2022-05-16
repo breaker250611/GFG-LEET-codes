827b212a342909ec67132efab4d46c150534af8c
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-05-16 16:59:24 +0530

    Time: 183 ms (22.17%), Space: 19.7 MB (22.41%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& times, int src, int dst, int k) {
+         vector<vector<pair<int,int>>>adj(n);
+        for(int i = 0;i<times.size();i++){
+        // cout<<"check";
+            int ui = times[i][0];
+            int vi = times[i][1];
+            int wi = times[i][2];
+            adj[ui].push_back({vi,wi});
+        }
+        vector<vector<int>>dist(101,vector<int>(101,INT_MAX));
+        vector<int> parent(n,-1);
+        dist[src][k] = 0;
+
+        using pii = pair<int,pair<int, int>>;
+    priority_queue<pii, vector<pii>, greater<pii>> q;
+        q.push({0,{src,k+1}});
+        while(!q.empty()){
+            int v = q.top().second.first;
+            int k = q.top().second.second;
+            int d_v = q.top().first;
+            q.pop();
+            if(d_v>=dist[v][k]) continue;
+                dist[v][k] = d_v;
+             for(auto edge:adj[v]){
+                int to = edge.first;
+                int len = edge.second;
+                 if(k==0) continue;
+                 // if(k<0) break;
+            
+
+                if(d_v+ len <dist[to][k-1]){
+                    // dist[to][k-1] = d_v +len;
+                    // parent[to ] = v;
+                    if(k>0){q.push({d_v+len,{to,k-1}});}
+                }
+            }
+        
+        }
+        int ans = INT_MAX;
+        for(int i = 0;i<=100;i++){
+            if(ans>dist[dst][i]){
+                ans = dist[dst][i];
+            }
+        }
+        if(ans==INT_MAX) return -1;
+        return ans;
+    }
+};
