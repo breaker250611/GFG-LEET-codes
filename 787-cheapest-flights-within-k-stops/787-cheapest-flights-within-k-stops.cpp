a5b9aaffbf7207243bdcd2eedb77a0b52dfc10d4
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-05-16 17:32:56 +0530

    Time: 98 ms (26.67%), Space: 16.8 MB (25.57%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -11,7 +11,7 @@ public:
         }
         vector<vector<int>>dist(101,vector<int>(101,INT_MAX));
         vector<int> parent(n,-1);
-        dist[src][k] = 0;
+        dist[src][k+1] = 0;
 
         using pii = pair<int,pair<int, int>>;
     priority_queue<pii, vector<pii>, greater<pii>> q;
@@ -23,6 +23,8 @@ public:
             q.pop();
             if(d_v>dist[v][k]) continue;
                 // dist[v][k] = d_v;
+            cout<<"d_v= "<<d_v<<endl;
+            cout<<"dist[v][k]"<<dist[v][k]<<endl;
              for(auto edge:adj[v]){
                 int to = edge.first;
                 int len = edge.second;
