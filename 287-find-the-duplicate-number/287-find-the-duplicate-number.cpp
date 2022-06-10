608aea0ff794451517e3b941b61bb908781b6930
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-06-10 10:07:01 +0530

    Time: 175 ms (48.54%), Space: 61.3 MB (27.94%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool check(vector<int> &arr , int m){
+    bool chk(vector<int> &arr,int m){
         int count = 0;
-        for (int i = 0 ; i < arr.size() ; i++){
-            arr[i] <= m ? count++ : count;
+        for(int i = 0;i<arr.size();i++){
+            arr[i] <= m?count++:count;
         }
         return count > m;
     }
-    
     int findDuplicate(vector<int>& nums) {
         
-        int low = 1 , high = nums.size()-1;
+        int low = 1;
+        int high = nums.size()-1;
         int ans = -1;
-        while (low <= high){
-            
+        while(low<=high){
             int mid = (low+high)/2;
-            bool ok = check(nums , mid);
-            
-            if (ok){
+            bool check = chk(nums,mid);
+            if(check){
                 ans = mid;
                 high = mid-1;
             }
-            else {
-               low = mid+1;   
-            }
+            else low = mid+1;
         }
         return ans;
     }
