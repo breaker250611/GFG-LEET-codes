301663ba5a362e34ab6ec20a5b8bccc052a53e03
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-06-13 14:58:34 +0530

    Time: 299 ms (41.43%), Space: 57.8 MB (77.92%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool bansaktahai(vector<int>& position, int m,int dis){
+        int prev = position[0];
+        m--;
+        for(int i = 0;i<position.size();i++){
+            if(position[i]-prev>=dis){
+                prev = position[i];
+                m--;
+            }
+        }
+        if(m<=0)return true;
+        return false;
+    }
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        int lo = 1;
+        int hi = position[position.size()-1];
+        
+        while(lo+1<hi){
+            int mid = lo +(hi-lo)/2;
+            if(bansaktahai(position,m,mid)){
+                lo = mid;
+            }
+            else{
+                hi  = mid;
+            }
+        }
+        return lo;
+    }
+};
