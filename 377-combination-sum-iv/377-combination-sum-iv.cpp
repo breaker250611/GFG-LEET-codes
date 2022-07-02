c3ac9bb25bdb9b13247e788d57d923ca0144052e
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-07-02 18:40:40 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (31.80%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int combinationSum4(vector<int>& nums, int target) {
-       
-        
-        vector<unsigned long long int>dp(target+1,0);
+    int combinationSum4(vector<int>& nums, int amount) {
+          vector<unsigned long long int>dp(amount+1,0);
         dp[0] = 1;
-        
-        for(unsigned long long int i = 1;i<=target;i++){
-            for(auto &it:nums){
-                if(it<=i){
-                    dp[i] +=dp[i-it];
+            for(unsigned long long int i = 1;i<amount+1;i++){
+        for(auto &it:nums){
+                if(i>=it){
+                    dp[i] += dp[i-it];
+                    // dp[i] += max(dp[i],temp);
                 }
             }
         }
-        return dp[target];
+        return dp[amount]= (dp[amount]==INT_MAX)?-1:dp[amount];
     }
 };
