9415ce04d329d860bb07d526fb91badfb1a2c0f0
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-07-19 21:54:42 +0530

    Time: 476 ms (92.76%), Space: 173.8 MB (52.35%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.cpp
@@ -0,0 +1,84 @@
+ struct Node{
+        int key;
+        int val;
+        Node *next;
+        Node *prev;
+        Node(){
+            key = 0;
+            val = 0;
+            next = NULL;
+            prev = NULL;
+        }
+        Node(int key,int value){
+            this->key = key;
+            this->val = value;
+            next = NULL;
+            prev = NULL;
+        }
+    };
+class LRUCache {
+    private : 
+    int cap;
+    unordered_map<int,Node*> mp;
+    Node *head = new Node();
+    Node *tail  = new Node();
+    void addNode(Node *node){
+        Node *temp = head->next;
+        head->next = node;
+        node->prev = head;
+        node->next = temp;
+        temp->prev = node;
+    }
+    void delNode(Node *node){
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
+    }
+public:
+   
+    LRUCache(int capacity) {
+        cap = capacity;
+        head ->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(mp.find(key)!=mp.end()){
+            Node *temp = mp[key];
+            int val = temp->val;
+            delNode(temp);
+            mp.erase(key);
+            addNode(temp);
+            mp[key] = head->next;
+            return val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){
+            Node* temp = mp[key];
+            temp->val = value;
+            delNode(temp);
+            mp.erase(key);
+
+            addNode(temp);
+            mp[key] = head->next;
+            return;
+        }
+        // cout<<cap<<endl;
+        if(mp.size() >= cap){
+            mp.erase(tail->prev->key);
+            delNode(tail->prev);
+        }
+        addNode(new Node(key,value));
+        mp[key] = head->next;
+        // if(mp.size()==4) cout<<"true"<<endl;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
