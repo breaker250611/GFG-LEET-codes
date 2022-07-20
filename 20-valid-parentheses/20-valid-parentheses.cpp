62576ae0b3e693560294cb0354e996574fd32c7b
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-07-20 10:44:49 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.10%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /*
+    1 = (
+    2 = {
+    3 = [
+    -ve for other
+    */
+    bool isValid(string s) {
+        stack<int>st;
+        map<char,int>mp = {{'(',1},{')',-1},{'[',2},{']',-2},{'{',3},{'}',-3}};
+        
+        for(int i =0;i<s.size();i++){
+            char ch = s[i];
+            int br = mp[ch];
+            if(st.size()==0 && br<0) return false;
+            if(br>0){
+                st.push(br);
+            }
+            else{
+                br = -1*br;
+                if(st.top()==br) st.pop();
+                else return false;
+            }
+        }
+        if(st.size()==0)return true;
+        return false;
+    }
+};
