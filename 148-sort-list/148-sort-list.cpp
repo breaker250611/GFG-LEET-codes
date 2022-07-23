204b245ead2d09c1b46ff08a0125b43e76dec560
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-07-23 12:10:30 +0530

    Time: 433 ms (14.01%), Space: 74.1 MB (29.52%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,39 +10,53 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if(head ==nullptr or head->next == nullptr) return head;
-        ListNode* mid = midnikal(head);
-        ListNode* left = sortList(head);
-        ListNode* right = sortList(mid);
-        return merge(left,right);
-     }
-    ListNode* merge(ListNode* list1, ListNode* list2) {
-        ListNode dummyHead(0);
-        ListNode* ptr = &dummyHead;
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                ptr->next = list1;
-                list1 = list1->next;
-            } else {
-                ptr->next = list2;
-                list2 = list2->next;
-            }
-            ptr = ptr->next;
+    ListNode* mid(ListNode* head){
+        ListNode* slow=head,*fast = head,*tail = head;
+        while(fast and fast->next){
+            tail = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        if(list1) ptr->next = list1;
-        else ptr->next = list2;
-
-        return dummyHead.next;
+        tail->next = NULL;
+        return slow;
     }
-    ListNode* midnikal(ListNode* head){
-          ListNode* midPrev = nullptr;
-        while (head && head->next) {
-            midPrev = (midPrev == nullptr) ? head : midPrev->next;
-            head = head->next->next;
+    ListNode *merge(ListNode *l1 , ListNode *l2){
+        
+        ListNode *D = new ListNode(-1);
+        ListNode *ptr = D;
+        
+        while (l1 and l2){
+            if (l1->val <= l2->val){
+                ptr->next = l1;
+                ptr = l1;
+                l1 = l1->next;
+            }
+            else {
+               ptr->next = l2;
+               ptr = l2;
+               l2 = l2->next;
+            }            
         }
-        ListNode* mid = midPrev->next;
-        midPrev->next = nullptr;
-        return mid;
+        
+        if (l1){
+            ptr->next = l1;
+        }
+        else if (l2){
+            ptr->next = l2;
+        }
+        return D->next;        
+    }
+    
+    ListNode* mergesort(ListNode* head){
+        if(!head or !head->next) return head;
+        
+        ListNode* midd = mid(head);
+        ListNode* pehla_half = mergesort(head);
+        ListNode* dusra_half = mergesort(midd);
+        
+        return merge(pehla_half , dusra_half);
+    }
+    ListNode* sortList(ListNode* head) {
+        return mergesort(head);
     }
 };
