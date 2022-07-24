7a9b7db7f97d36214e21b59a98bcba0fb778be20
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-07-24 15:10:53 +0530

    Time: 9 ms (31.30%), Space: 7.3 MB (66.61%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>st;
+        st.push(-1);
+        int maxi = 0;
+        
+        for(int i = 0;i<s.size();i++){
+            char ch = s[i];
+            if(ch =='(') st.push(i);
+            else {
+                    st.pop();
+                if(st.size()>0){
+                    maxi = max(maxi,i-st.top());
+                }else{
+                    st.push(i);
+                }
+            }
+        }
+        return maxi;
+    }
+};
