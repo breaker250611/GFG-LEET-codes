74039b610db840a7ce3bb513d8217a19afbf6932
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-01 11:41:59 +0530

    Time: 7 ms (35.79%), Space: 10.9 MB (36.40%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void call(vector<int>& nums,vector<int> daal,int i){
+    void call(vector<int>& nums,vector<int> &daal,int i){
         
         if(i==nums.size()){
             ans.push_back(daal);
@@ -11,11 +11,12 @@ public:
         call(nums,daal,i+1);
         daal.push_back(nums[i]);
         call(nums,daal,i+1);
+        daal.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> daal;
         ans.clear();
-        call(nums,{},0);
+        call(nums,daal,0);
         return ans;
     }
 };
