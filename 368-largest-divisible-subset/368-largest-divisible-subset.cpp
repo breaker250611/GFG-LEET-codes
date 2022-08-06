16466abe0d19b920af8619bf9eae39fc156c8d6e
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-06 10:30:02 +0530

    Time: 93 ms (34.53%), Space: 8.7 MB (92.49%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -3,41 +3,32 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<int>dp(nums.size(),1);
-        for(int i = 0;i<nums.size();i++){
+        vector<int>parent(nums.size(),0);
+        
+        int start = 0;
+        int maxi = 0;
+        for(int i = 1 ;i<nums.size();i++){
+            parent[i] = i;
             for(int j = 0;j<i;j++){
-                if(nums[i]>nums[j]&&dp[i]<dp[j]+1){
-                    if(nums[i]%nums[j]==0||nums[j]%nums[i]==0){
+                
+                if(nums[i]%nums[j]==0){
+                    if(dp[i]<dp[j]+1){
                         dp[i] = dp[j]+1;
+                        parent[i] = j;
                     }
+                }
+                if(maxi<dp[i]){
+                    maxi = dp[i];
+                    start = i;
                 }
             }
         }
         vector<int>ans;
-        int max =*max_element(dp.begin(),dp.end()); 
-        cout<<max<<endl;
-        for(int i = nums.size()-1;i>=0&&max>0;i--){
-            int ans1 = 0;
-            if(ans.size()!=0)
-                  ans1 = ans.back();
-            if(max==dp[i]){
-                max--;
-                ans.push_back(nums[i]);
-    
-               
-            }
-            int ans2 =0;
-            // cout<<ans1<<" "<<ans2<<endl;
-            if(ans.size()!=0)
-            ans2 = ans.back();
-            cout<<ans1<<" "<<ans2<<endl;
-
-            if((ans1!=0||ans2!=0)&& (ans1%ans2!=0&&ans2%ans1!=0)) {
-                ans.pop_back();
-                max++;
-            }
+        while(parent[start]!=start){
+            ans.push_back(nums[start]);
+            start = parent[start];
         }
-        reverse(ans.begin(),ans.end());
-
+        ans.push_back(nums[start]);
         return ans;
     }
 };
