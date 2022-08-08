a75344447a0ac9b2a5316fa147a78e6e35eb0a04
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-08 11:13:49 +0530

    Time: 14 ms (15.18%), Space: 7.3 MB (21.18%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        vector<int>pos(nums.size(),1),neg(nums.size(),1);
-   for(int i = 0;i<nums.size();i++){
-       for(int j = 0;j<i;j++){
-           if(nums[i]-nums[j]==0) continue;
-           else if(nums[i]-nums[j]>0){
-               pos[i]= max(pos[i],neg[j]+1);
-           }
-           else if(nums[i]-nums[j]<0){
-               neg[i]= max(neg[i],pos[j]+1);
-           }
-
-       }
-   }
-        int m = *max_element(neg.begin(),neg.end());
-        int n = *max_element(pos.begin(),pos.end());
-        return max(n,m);
+        int n = nums.size();
+        vector<int> pos(n, 1), neg(n, 1);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                int diff = nums[i] - nums[j];
+                if (diff == 0) 
+                    continue; 
+                else if (diff > 0) 
+                    pos[i] = max(pos[i], neg[j] + 1);
+                else 
+                    neg[i] = max(neg[i], pos[j] + 1);
+            }
         }
+        return max(pos.back(), neg.back());
+    }
 };
