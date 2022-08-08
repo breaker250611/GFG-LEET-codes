71fb1e2eede8c810aad3e21ec9f9a541e27cd068
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-08 21:55:52 +0530

    Time: 17 ms (29.23%), Space: 13.6 MB (84.19%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-    
-    int binarysearch_first(vector<int> &nums , int low , int high , int target){
-        
-        int ans = -1;
-        while (low <= high){
-            
-            int mid = (low + high)/2;
-            
-            if (nums[mid] == target){
-                ans = mid;
-                high = mid-1;
-            }
-            else if (nums[mid] > target)
-                high = mid-1;
-            else
-                low = mid+1;
-        }
-        return ans;  
-    }
-    
-    int binarysearch_second(vector<int> &nums , int low , int high , int target){
-        
-        int ans = -1;
-        while (low <= high){
-            
-            int mid = (low + high)/2;
-            
-            if (nums[mid] == target){
-                ans = mid;
-                low = mid+1;
-            }
-            else if (nums[mid] < target)
-                low = mid+1;
-            else
-                high = mid-1;
-        }
-        return ans;  
-      
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-     
-        // 0 for front ... //
-        int first = binarysearch_first(nums , 0 , nums.size()-1 , target);
-        // 1 for back  ... //
-        int second = binarysearch_second(nums , 0 , nums.size()-1 , target);
-        return {first , second};
+        vector<int>ans = {-1,-1};
+        if(nums.size()==0)return ans;
+        int lo = 0,hi=nums.size()-1;
+        
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(nums[mid] == target){
+                ans[0] = mid;
+                hi = mid-1;
+            }
+            else if(nums[mid]>target) hi = mid-1;
+            else lo = mid+1;
+        }
+        
+        
+        if(ans[0]==-1) return ans;
+        
+        
+        lo = 0,hi = nums.size()-1;
+        
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+             if(nums[mid] == target){
+                ans[1] = mid;
+                lo = mid+1;
+            }
+            else if(nums[mid]>target) hi = mid-1;
+            else lo = mid+1;
+            
+        }
+            // cout<<"checl"<<endl;
+        return ans;
     }
 };
