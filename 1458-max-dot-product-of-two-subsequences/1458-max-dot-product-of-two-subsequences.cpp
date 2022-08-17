cea9a80f9dd8da35a0e3883c1b4d209361e03712
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-17 17:24:13 +0530

    Time: 65 ms (44.76%), Space: 13.4 MB (13.14%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,33 +1,20 @@
 class Solution {
-    public:
-    vector<int> arr, brr;
-    vector<vector<int>> dp; 
-    int n, m;
-
-    int fun(int i, int j) {
-        if (i == n or j == m) {
-            return -1e5;
-        }
-
-        int &ans = dp[i][j]; 
-        if (ans == -1) {
-            ans = max({
-                arr[i] * brr[j], 
-                fun(i + 1, j + 1) + arr[i] * brr[j], 
-                fun(i, j + 1), 
-                fun(i + 1, j)
-            });
+public:
+    vector<vector<int>>dp;
+    int sol(vector<int>&nums1,vector<int>&nums2,int i ,int  j ){
+        if(i>=nums1.size() or j>=nums2.size()) return -1e4;
+        
+        int &ans = dp[i][j];
+        if(ans==INT_MIN){
+            long long  peh = max(nums1[i]*nums2[j] , nums1[i]*nums2[j]+sol(nums1,nums2,i+1,j+1));
+            long long  dus = sol(nums1,nums2,i+1,j);
+            long long  tees = sol(nums1,nums2,i,j+1);
+            ans = max({peh,dus,tees});
         }
         return ans;
     }
-
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        n = nums1.size(); 
-        m = nums2.size();
-        arr = nums1; 
-        brr = nums2;
-        dp.resize(n + 1, vector<int>(m + 1, -1));
-
-        return fun(0, 0);
+        dp.resize(nums1.size()+1,vector<int>(nums2.size()+1,INT_MIN));
+        return sol(nums1,nums2,0,0);
     }
 };
