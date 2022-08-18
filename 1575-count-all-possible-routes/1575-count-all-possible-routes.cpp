b83e1db76d47ef31d2ff76818bcc8183e0901616
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-18 20:12:48 +0530

    Time: 410 ms (27.43%), Space: 12.1 MB (30.61%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,33 +1,43 @@
+const long long int mod= 1e9+7; 
 class Solution {
-     int mod = 1000000007;
 public:
-    int n , target;
-   
-    int solve (vector<int>&a , int i , int fuel , vector<vector<int>>&dp){
-        
-        int op = a[i] == target;
-        if (fuel <= 0){
-            return op;
+    vector<vector<int>>dp;
+    
+    int call(vector<int>& locations, int start,int finish,int fuel){
+        if(start==finish and fuel==0) return 1;
+        else if(start==finish and fuel>0){
+            int &temp = dp[start][fuel];
+            if(temp == -1)
+            { temp = 1;
+              for(int i = 0 ;i<locations.size();i++){
+                if(i==start)continue;
+                int minus = abs(locations[i]-locations[start]);
+                temp =(temp%mod + call(locations,i,finish,fuel-minus)%mod)%mod;
+            }
+            }
+            return temp;
         }
+        if(start==locations.size() or fuel<0) return 0;
         
-        if (dp[i][fuel] != -1)
-            return dp[i][fuel];
-        
-        op = a[i] == target;
-        for (int k = 0 ; k<a.size() ; k++){
-            if (k != i){
-                if (fuel-abs(a[i]-a[k]) >= 0){
-                  op = (op + solve(a , k , fuel-abs(a[i]-a[k]) , dp))%mod;
-                }
+        int &ans = dp[start][fuel];
+        if(ans ==-1){
+            ans = 0;
+            for(int i = 0 ;i<locations.size();i++){
+                if(i==start)continue;
+                int minus = abs(locations[i]-locations[start]);
+                ans = ((ans)%mod +(call(locations,i,finish,fuel-minus))%mod)%mod;
             }
         }
-        return dp[i][fuel] = op;
+        return ans;
     }
-    
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        n = locations.size();
-        target = locations[finish];
-        vector<vector<int>> dp(n+1 , vector<int>(fuel+1 , -1));
-        return solve(locations , start , fuel , dp);
+        dp.resize(locations.size()+1,vector<int>(fuel+1,-1));
+        return call(locations,start,finish,fuel);
+        int count = 0;
+        for(int i = 0;i<=fuel;i++){
+            
+            if(dp[finish][i] !=-1) count +=dp[finish][i];
+        }
+        return count;
     }
 };
