6cb1ea2715d0c337ced6f07ada1eb31bf215abb3
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-26 22:32:53 +0530

    Time: 13 ms (67.16%), Space: 10.5 MB (6.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-      vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),0));
+    vector<vector<int>>grid;
+    vector<vector<int>>dp;
+    int call(int i ,int j){
         
-        for(int i = 0;i<grid.size();i++){
-            for(int j =0;j<grid[0].size();j++){
-                if(i==0&&j==0) dp[i][j] = grid[i][j];
-                else if(i==0) dp[i][j] = dp[i][j-1]+grid[i][j];
-                else if(j==0) dp[i][j] = dp[i-1][j]+grid[i][j];
-                else{
-                    dp[i][j] = min(dp[i-1][j],dp[i][j-1])+grid[i][j];
-                }
-            }
+        if(i==0 and j==0){
+            return dp[i][j] = grid[i][j];
         }
-        return dp[grid.size()-1][grid[0].size()-1];
+        else if(i<0 or j<0)return INT_MAX;
+        int &ans = dp[i][j];
+        if(ans != -1)return ans;
+        
+        ans = min(call(i-1,j),call(i,j-1))+grid[i][j];
+        return ans;
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        this->grid = grid;
+        dp.resize(grid.size(),vector<int>(grid[0].size(),-1));
+       return call(grid.size()-1,grid[0].size()-1);
+        // for(auto &it:dp){
+        //     for(auto &i:it)cout<<i<<" ";
+        //     cout<<endl;
+        // }
+        
+        return 0;
     }
 };
