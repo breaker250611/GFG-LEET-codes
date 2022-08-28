402525a84bfe418dec48bce31f5958b6c0f59aba
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2022-08-28 12:44:09 +0530

    Time: 76 ms (38.38%), Space: 10.5 MB (23.45%) - LeetHub

---
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int n ;
+    int call(int x,int y,string &str,int i){
+        if(x<0 or y<0 or x>=n or y>=n)return -1;
+        if(i>=str.size())return 0;
+        char ch = str[i];
+        if(ch=='R'){
+            return 1+call(x,y+1,str,i+1);
+        }
+        else if(ch=='L'){
+            return 1+call(x,y-1,str,i+1);
+            
+        }
+        else if(ch=='U'){
+            
+            return 1+call(x-1,y,str,i+1);
+        }
+        else if(ch=='D'){
+            return 1+call(x+1,y,str,i+1);
+            
+        }
+        return 0;
+    }
+    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
+        this-> n = n;
+        vector<int>ans;
+            int x = startPos[0];
+            int y = startPos[1];
+        for(int i = 0;i<s.size();i++){
+            // string temp = s.substr(i);
+            int cnt = call(x,y,s,i);
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
