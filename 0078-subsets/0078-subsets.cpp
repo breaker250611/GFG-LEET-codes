d6792c2319c373f80e2a303441cfd7d0df9430a4
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2023-04-19 21:24:05 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (73.12%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     vector<vector<int>>ans;
     
-    void solve(vector<int>&nums , vector<int>  temp,int i ){
+    
+    void solve(vector<int>&nums,vector<int>&temp,int i){
         
-        if(i == nums.size()){
             ans.push_back(temp);
-            return;
-        }
-            
-            
-            
-            solve(nums,temp,i+1);
-            temp.push_back(nums[i]);
-            solve(nums,temp,i+1);
-            
         
+        for(int j = i;j<nums.size();j++){
+            temp.push_back(nums[j]);
+            solve(nums,temp,j+1);
+            temp.pop_back();
+        }
     }
-    
-    
     vector<vector<int>> subsets(vector<int>& nums) {
+        
         vector<int>temp;
-        ans.clear();
         solve(nums,temp,0);
         return ans;
     }
+    
 };
