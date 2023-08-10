105521ba49f8e2f9f29bf12b27679ac1bbe4c83f
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2023-08-10 21:49:03 +0530

    Time: 668 ms (86.36%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,5 +1,3 @@
-SELECT (CASE WHEN MOD(id,2)=1 AND id!=(SELECT COUNT(*) FROM seat) THEN id+1
-WHEN MOD(id,2)=0 THEN id-1
-ELSE id END)id, student
-FROM seat
-ORDER BY id;
+# Write your MySQL query statement below
+SELECT s1.id,s2.student from Seat s1 cross join Seat s2 where (s1.id Mod 2=0 and s1.id-1=s2.id) or (s1.id MOD 2 = 1 and s1.id+1 = s2.id) or (s1.id = (select count(id) from seat) and s2.id = s1.id
+ and s1.id MOD 2 = 1) order by 1; 
