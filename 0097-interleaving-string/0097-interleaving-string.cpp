6a13ccf9b88e163dee20854d5472adb21443753d
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2023-08-25 12:53:44 +0530

    Time: 30 ms (29.45%), Space: 80.8 MB (8.55%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -3,7 +3,11 @@ public:
         vector<vector<vector<int>>>dp;
     
     int call (string &s1,string &s2,string &s3,int i , int j, int k){
-        if(i==s1.size() and j == s2.size()) return 1;
+        if(i==s1.size() and j == s2.size()) {
+            dp[i][j][k] = 1;
+            return dp[i][j][k];
+        
+        }
         int &ans = dp[i][j][k];
         if(ans ==-1){
             ans = 0;
@@ -20,6 +24,7 @@ public:
         dp.resize(s1.size()+1,vector<vector<int>>(s2.size()+1,vector<int>(s3.size()+1,-1)));
         if((s1.size()+s2.size())!=s3.size() ) return false;
         int ans = call(s1,s2,s3,0,0,0);
+        ans = dp[s1.size()][s2.size()][s3.size()];
         if(ans==1)return true;
         return false;
     }
