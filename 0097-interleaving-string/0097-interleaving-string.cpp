5700328d2f01a6b383dd2e73cc8d4a6807c99326
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2023-08-25 12:50:33 +0530

    Time: 24 ms (34.01%), Space: 80.8 MB (8.55%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+        vector<vector<vector<int>>>dp;
+    
+    int call (string &s1,string &s2,string &s3,int i , int j, int k){
+        if(i==s1.size() and j == s2.size()) return 1;
+        int &ans = dp[i][j][k];
+        if(ans ==-1){
+            ans = 0;
+            if(s1[i]==s3[k]){
+                ans = max(ans,call(s1,s2,s3,i+1,j,k+1));
+            }
+            if(s2[j]==s3[k]){
+                ans = max(ans,call(s1,s2,s3,i,j+1,k+1));
+            }
+        }
+        return ans;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        dp.resize(s1.size()+1,vector<vector<int>>(s2.size()+1,vector<int>(s3.size()+1,-1)));
+        if((s1.size()+s2.size())!=s3.size() ) return false;
+        int ans = call(s1,s2,s3,0,0,0);
+        if(ans==1)return true;
+        return false;
+    }
+};
