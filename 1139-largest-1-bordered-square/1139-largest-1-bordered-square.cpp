7ffed00f2242c2b64b54cf1f6f7f475918652f7f
breaker250611 <69757141+breaker250611@users.noreply.github.com>
2023-09-04 10:29:12 +0530

    Time: 10 ms (91.59%), Space: 11.7 MB (40.65%) - LeetHub

---
+++ 1139-largest-1-bordered-square/1139-largest-1-bordered-square.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int largest1BorderedSquare(vector<vector<int>>& grid) {
+        vector<vector<int>>dph(grid.size(),vector<int>(grid[0].size(),0));
+        vector<vector<int>>dpv(grid.size(),vector<int>(grid[0].size(),0));
+        
+        
+        for(int i = 0;i<grid.size();i++){
+            for(int j = 0;j<grid[0].size();j++){
+                
+                if(grid[i][j]){
+                    
+                    dph[i][j] = grid[i][j];
+                    if(j>0){
+                        dph[i][j] +=dph[i][j-1];
+                    }
+                    dpv[i][j] = grid[i][j];
+                    if(i>0){
+                        dpv[i][j] += dpv[i-1][j];
+                    }
+                    
+                }
+            }
+        }
+        int ans = 0;
+        
+        for(int i = grid.size()-1;i>=0;i--){
+            for(int j = grid[0].size()-1;j>=0;j--){
+                int temp = min(dph[i][j],dpv[i][j]);
+                
+                while(temp>ans){
+                    if(dph[i-temp+1][j]>=temp and dpv[i][j-temp+1]>=temp){
+                        ans = temp;
+                     break;   
+                    }
+                    temp--;
+                }
+            }
+        }
+        
+        
+        return ans*ans;
+    }
+};
